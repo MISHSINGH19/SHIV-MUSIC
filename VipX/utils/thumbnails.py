bd6cb1c5d2d7238a1c0603ea2768c0afda7feb59
MISHSINGH19 <141798577+MISHSINGH19@users.noreply.github.com>
2023-09-10 16:59:58 +0530

    Update thumbnails.py

--- VipX/utils/thumbnails.py
+++ VipX/utils/thumbnails.py
@@ -113,7 +113,7 @@ async def gen_thumb(videoid, chat_id):
         width = int((1280 - 1) / 2)
         background = Image.open(f"cache/temp{videoid}.png")
         background.paste(logo, (width + 2, 1380), mask=logo)
-        background.paste(x, (900, 727), mask=x)
+        background.paste(x, (900, 627), mask=x)
         background.paste(image3, (0, 0), mask=image3)
 
         draw = ImageDraw.Draw(background)
