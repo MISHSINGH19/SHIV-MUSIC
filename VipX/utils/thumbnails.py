5a668907876db515733a543d8c3081e157a1286c
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-09-15 21:00:30 +0530

    Update thumbnails.py

--- VipX/utils/thumbnails.py
+++ VipX/utils/thumbnails.py
@@ -83,7 +83,7 @@ async def gen_thumb(videoid, chat_id):
 
         youtube = Image.open(f"cache/thumb{videoid}.png")
         bg = Image.open(f"VipX/assets/bg.png")
-        image1 = changeImageSize(1280, 720, youtube)
+        image1 = changeImageSize(1280, 520, youtube)
         image2 = image1.convert("RGBA")
         background = image2.filter(filter=ImageFilter.BoxBlur(1.2))
         enhancer = ImageEnhance.Brightness(background)
